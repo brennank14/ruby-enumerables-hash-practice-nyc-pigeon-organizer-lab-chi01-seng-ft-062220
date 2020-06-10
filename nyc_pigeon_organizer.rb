2583361f255a44ca8684e4f93117503cad7d63ad
Kathleen Brennan <brennank14@gmail.com>
2020-06-10 22:57:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,14 +6,15 @@ def nyc_pigeon_organizer(data)
       names.each do |name|
         if !final_array[name]
           final_array[name] = {}
-          #binding.pry
+          binding.pry
         end
         if !final_array[name][key]
           final_array[name][key]=[]
+          binding.pry
         end
       final_array[name][key].push(inner_key)
       end
     end
   end
-  binding.pry 
+  binding.pry
 end
