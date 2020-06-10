9f62e018b170a989d6c1cab038a6cd1363709f07
Kathleen Brennan <brennank14@gmail.com>
2020-06-10 22:47:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,12 +8,11 @@ def nyc_pigeon_organizer(data)
           final_array[name] = {}
         end
         if !final_array[name][key]
-          final_array[names][key]=[]
+          !final_array[names][key]=[]
         end
       final_array[name][key].push(inner_key)
       end
-      binding.pry
     end
   end
-
+  binding.pry 
 end
