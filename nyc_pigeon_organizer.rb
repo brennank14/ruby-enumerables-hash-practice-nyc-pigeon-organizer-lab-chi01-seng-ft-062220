d6423e15dd13c40225797f24d8658b8e8757a60c
Kathleen Brennan <brennank14@gmail.com>
2020-06-10 22:29:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  data.each_with_object({}) do |(key, value), final_array|
+  final results = data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, names|
       names.each do |name|
         #binding.pry
@@ -15,4 +15,5 @@ def nyc_pigeon_organizer(data)
       end
     end
   end
+  binding.pry
 end
