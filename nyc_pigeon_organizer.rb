eab2caef6d05c6259e1e1cb238c829222c566ad5
Kathleen Brennan <brennank14@gmail.com>
2020-06-10 22:31:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,6 @@ def nyc_pigeon_organizer(data)
   final results = data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, names|
       names.each do |name|
-        #binding.pry
         if !final_array[name]
           final_array.names = {}
         end
